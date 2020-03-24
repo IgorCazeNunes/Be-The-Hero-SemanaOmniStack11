9ac0658d2c6d2e25edd15a9b75d867df98b28f39
Igor Cazé Nunes <igorcaze@gmail.com>
2020-03-24 12:14:08 -0300

    Adicionado IncidentController.
    Adicionado route Index Incident.
    Adicionado route Create Incident.
    Adicionado route DeleteIncident.

--- backend/src/database/migrations/20200324105924_create_incidents.js
+++ backend/src/database/migrations/20200324105924_create_incidents.js
@@ -1,17 +1,16 @@
-
-exports.up = function(knex) {
-    return knex.schema.createTable('incidents', function (table) {
+exports.up = function (knex) {
+    return knex.schema.createTable("incidents", function (table) {
         table.increments();
+        
+        table.string("title").notNullable();
+        table.string("description").notNullable();
+        table.decimal("value").notNullable();
 
-        table.string('title').notNullable();
-        table.string('description').notNullable();
-        table.decimal('value').notNullable();
+        table.string("ong_id").notNullable();
+        table.foreign("ong_id").references("id").inTable("ongs");
+    });
+}
 
-        table.string('ong_id').notNullable();
-        table.foreign('ong_id').references('id').inTable('ongs');
-    })
-};
-
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema.dropTable('incidents');
 };
