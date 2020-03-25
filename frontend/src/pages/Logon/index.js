fbdef000e841de4a5beabae7823d204a3c8a13d4
Igor Cazé Nunes <igorcaze@gmail.com>
2020-03-25 15:24:19 -0300

    Alterações no css da page Logon.
    Adicionado espaçamento ao container.
    Generalizado a estilização do link para o global css.

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -20,7 +20,7 @@ export default function Logon() {
 
                     <button type="submit" className="button">Entrar</button>
 
-                    <Link to="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041" />
                         Não tenho cadaastro.
                     </Link>
