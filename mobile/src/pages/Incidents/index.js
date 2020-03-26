875a1be2cd63f3652e26a291e9ad6442a2df7913
Igor Cazé Nunes <igorcaze@gmail.com>
2020-03-26 14:34:49 -0300

    Adicionado scroll infinito a page Incidents.

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -11,15 +11,32 @@ export default function Incidents() {
     const navigation = useNavigation();
     const [incidents, setIncidents] = useState([]);
     const [total, setTotal] = useState(0);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
 
     function navigateToDetail(incident) {
         navigation.navigate('Detail', { incident });
     }
 
     async function loadIncidents() {
-        const response = await api.get('/incidents');
-        setIncidents(response.data);
+        if(loading) {
+            return;
+        }
+
+        if (total > 0 && incidents.lenght == total) {
+            return;
+        }
+
+        setLoading(true);
+
+        const response = await api.get('/incidents', {
+            params: { page }
+        });
+        setIncidents([...incidents, ...response.data]);
         setTotal(response.headers['x-total-count']);
+
+        setPage(page + 1);
+        setLoading(false);
     }
 
     useEffect(() => {
@@ -46,9 +63,10 @@ export default function Incidents() {
 
             <FlatList 
                 style={styles.incidentList}
-                showsVerticalScrollIndicator={false}
                 data={incidents}
                 keyExtractor={incident => String(incident.id)}
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.2}
                 renderItem={({ item: incident }) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
